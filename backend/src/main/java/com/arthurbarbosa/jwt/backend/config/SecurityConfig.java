86d76eb9734d9d4dc2b64fa6c97e007120150ba5
Arthur Barbosa <arthurbruno03@gmail.com>
2023-09-11 19:19:43 -0400

    Created login and register user frontend

--- backend/src/main/java/com/arthurbarbosa/jwt/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/arthurbarbosa/jwt/backend/config/SecurityConfig.java
@@ -10,20 +10,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserAuthenticationEntryPoint userAuthenticationEntryPoint;
+    private final UserAuthenticationProvider userAuthenticationProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.exceptionHandling(customizer -> customizer.authenticationEntryPoint(userAuthenticationEntryPoint))
+                .addFilterBefore(new JwtAuthFilter(userAuthenticationProvider), BasicAuthenticationFilter.class).
+                csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((requests) ->
-                        requests.requestMatchers(HttpMethod.POST, "/login").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/register").permitAll()
+                        requests.requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
                                 .anyRequest().authenticated()
                 );
         return http.build();
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthContentComponent } from './auth-content/auth-content.component';
+import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ContentComponent } from './content/content.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AuthContentComponent,
+    WelcomeContentComponent,
+    LoginFormComponent,
+    ContentComponent,
+    ButtonsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
