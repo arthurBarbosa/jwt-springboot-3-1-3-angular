0dbaa5993dc2ed1929182298c7a3748179653c0c
Arthur Barbosa <arthurbruno03@gmail.com>
2023-09-15 20:50:43 -0400

    Created domain class Payment

--- backend/src/main/java/com/arthurbarbosa/jwt/backend/entities/Payment.java
+++ backend/src/main/java/com/arthurbarbosa/jwt/backend/entities/Payment.java
@@ -1,6 +1,5 @@
 package com.arthurbarbosa.jwt.backend.entities;
 
-import com.arthurbarbosa.jwt.backend.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,19 +13,17 @@ import java.time.Instant;
 @Builder
 @Data
 @Entity
-@Table(name = "orders")
-public class Order {
+@Table(name = "payment")
+public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant moment;
-    private OrderStatus status;
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
 
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
 }
