d86b17b7a49a1101936fbfa778bd761a14c4bfe8
Arthur Barbosa <arthurbruno03@gmail.com>
2023-10-01 11:43:34 -0400

    List all paginated products

---
+++ backend/src/main/java/com/arthurbarbosa/jwt/backend/mappers/ProductMapper.java
@@ -0,0 +1,11 @@
+package com.arthurbarbosa.jwt.backend.mappers;
+
+import com.arthurbarbosa.jwt.backend.dtos.ProductDTO;
+import com.arthurbarbosa.jwt.backend.entities.Product;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    ProductDTO toProductDto(Product entity);
+}
